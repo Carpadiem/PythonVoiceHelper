ddb8f6997f317d98041df86961325d60799d11b7
Head-Cutter <egorovkirill2003@yandex.ru>
2020-11-12 17:28:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def myCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print('Say something...')
-        r.pause_threshold = 1
+        r.pause_threshold = 1 #ssas
         r.adjust_for_ambient_noise(source, duration=1)
         audio = r.listen(source)
     try:
